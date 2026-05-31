500afe36c84c0e2f02477a68493abacc944832ab
o-svaaa <olesya.somova06@gmail.com>
2026-05-31 20:01:11 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,9 +1,15 @@
 <?php
+// В самом начале файла api/index.php
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+header('Access-Control-Allow-Credentials: true');
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
